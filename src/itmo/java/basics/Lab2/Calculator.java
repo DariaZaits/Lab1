3dbf71c0c09dc03145edc05f6a5bcd93a009064b
DariaZaits <118551007+DariaZaits@users.noreply.github.com>
2022-11-20 14:42:00 +0300

    Добавлена ветка Lab2 и класс Calculator

---
+++ src/itmo/java/basics/Lab2/Calculator.java
@@ -0,0 +1,74 @@
+package itmo.java.basics.Lab2;
+
+public class Calculator {
+    void sum( int a, int b)
+    {
+        int c = a+b;
+        System.out.println(c);
+
+    }
+    void sum( double a, double b)
+    {
+        double c = a+b;
+        System.out.println(c);
+    }
+    void sum( long a, long b)
+    {
+        long c = a+b;
+        System.out.println(c);
+
+    }
+
+    void subtract (int a, int b)
+    {
+        int c = a-b;
+        System.out.println(c);
+
+    }
+    void subtract (double a, double b)
+    {
+        double c = a-b;
+        System.out.println(c);
+    }
+    void subtract (long a, long b)
+    {
+        long c=a-b;
+        System.out.println(c);
+    }
+
+    void multiplication (int a, int b)
+    {
+        int c = a*b;
+        System.out.println(c);
+
+    }
+    void multiplication (double a, double b)
+    {
+        double c = a*b;
+        System.out.println(c);
+    }
+    void multiplication (long a, long b)
+    {
+        long c=a*b;
+        System.out.println(c);
+    }
+
+    void division (int a, int b)
+    {
+        int c = a*b;
+        System.out.println(c);
+
+    }
+    void division (double a, double b)
+    {
+        double c = a*b;
+        System.out.println(c);
+    }
+    void division (long a, long b)
+    {
+        long c=a*b;
+        System.out.println(c);
+    }
+
+
+}
